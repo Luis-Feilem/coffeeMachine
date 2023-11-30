aa13a7da7a8e9085e1012a37fb28bdee2409f00d
Luis-Feilem <l.martin.roldan.cervantes@gmail.com>
2023-11-30 15:42:51 +0100

    Main moved to own file. Added custom levels to ingredients in order

--- coffee_machine.py
+++ coffee_machine.py
@@ -12,113 +12,119 @@ class CoffeeMachine:
     deliver_state = 4
     exit_state = -1
 
-    def __init__(self, catalogue):
-        self.catalogue = catalogue
+    def __init__(self, catalogue: [Recipe]):
+        self.catalogue = {}
+        for r in catalogue:
+            self.catalogue[r.name] = r
         self.state = self.init_state
 
     def init_sell(self):
         self.order = None
-        self.selection_valid = False
-        self.state = self.wait_state
+        return self.wait_state
     
     def start_screen(self):
-        ### Create welcoming GUI
         sg.theme('Purple')
         layout = [ [sg.Text("Welcome to the coffe vending machine at Metaforum, floor 6")],
                   [sg.Text("Here are the available options for your coffe: ")]
         ]
-        for o in self.catalogue.values():
-            layout.append([sg.Text(o)])
+        for r in self.catalogue.values():
+            layout.append([sg.Text(r.to_string())])
             
-        layout.append([sg.Text("Enter something on Row 2"), sg.InputText()])
+        layout.append([sg.Text("Enter the option you would like to have (case sensitive)"), sg.InputText()])
         layout.append([sg.Button("Order"), sg.Button("Exit")])
 
         startWindow = sg.Window("MetaCoffee", layout)
         
         ### Manage GUI interactions
-        while self.state == self.wait_state: 
+        out_state = self.wait_state
+        while out_state == self.wait_state: 
             event, values = startWindow.read()
-            if event == "Exit":
-                startWindow.close()
-                self.state = self.exit_state
-                break
-            if event == sg.WIN_CLOSED:
-                self.state = self.exit_state
-                break
+            if event == "Exit" or event == sg.WIN_CLOSED:
+                out_state = self.exit_state
             if event == "Order":
-                self.select_order(values[0])
+                out_state = self.select_order(values[0])
         startWindow.close()
-        
+        return out_state
 
-    def read_catalogue(self):
-        for r in self.catalogue.values():
-            print(r)
+    def read_recipe(r):
+        return r.to_string()
 
     def select_order(self, selection):
         if selection in self.catalogue.keys():
             self.order = Order(self.catalogue[selection])
-            self.selection_valid = True
-            self.state = self.confirm_state
-        if not self.selection_valid:
+            if self.order.is_customizable():
+                return self.customize_order()
+            else:
+                print("not customizable")
+                return self.confirm_state
+        else:
             sg.popup("Your selection was not valid, please enter the name of one of our offers.")
+            return self.wait_state
 
+    def customize_order(self):
+        # TODO
+        layout = []
+        for co in self.order.recipe.custom_options:
+            layout.append([sg.Text("Select how much " + co[0] + " you would like ("+ str(co[1])+ ")"), sg.InputText(co[2])])
+        layout.append([sg.Button("Confirm"), sg.Button("Cancel")])
+        
+        customWindow = sg.Window("Customize values", layout,modal=True)
+        out_state = self.wait_state
+        while out_state == self.wait_state:
+            event, values = customWindow.read()
+            if event == "Cancel" or event == sg.WIN_CLOSED:
+                break
+            if event == "Confirm":
+                out_state = self.confirm_state
+        customWindow.close()
+        return out_state
 
     def confirm_and_pay_order(self):
         layout = [[sg.T("This is your order: " + self.order.to_string() + ". Please, proceed to payment now.")],
                   [sg.Button("Pay"), sg.Button("Cancel")]
         ]
+        
         payWindow = sg.Window("Payment protocol", layout,modal=True)
-        while self.state == self.confirm_state:
+        out_state = self.confirm_state
+        while out_state == self.confirm_state:
             events, values = payWindow.read()
             if events == "Pay":
-                self.pay_order()
-                self.state = self.prepare_state
+                out_state = self.pay_order()
             else:
-                self.state = self.wait_state
+                out_state = self.wait_state
         if not payWindow.is_closed():
             payWindow.close()
-            
-        return
+        return out_state
     
     def pay_order(self):
+        # TODO
         self.order.is_paid(True)
+        return self.prepare_state
         
     def prepare_order(self):
-        self.state = self.deliver_state
+        # TODO
+        # Look at order custom options and prepare accordingly
+        
+        return self.deliver_state
         
     def deliver_coffee(self):
+        # TODO?
         sg.popup("Here is your coffee. Thank you for using our services.")
-        self.state = self.init_state
+        return self.init_state
         
     def sell_coffee(self):
         while self.state != self.exit_state:
             match self.state:
                 case self.init_state:
-                    self.init_sell()
+                    self.state = self.init_sell()
                 case self.wait_state:
-                    self.start_screen()
+                    self.state = self.start_screen()
                 case self.confirm_state:
-                    self.confirm_and_pay_order()
+                    self.state = self.confirm_and_pay_order()
                 case self.prepare_state:
-                    self.prepare_order()
+                    self.state = self.prepare_order()
                 case self.deliver_state:
-                    self.deliver_coffee()
+                    self.state = self.deliver_coffee()
 
 
-def main():
-    catalogue = createCatalogue()
-    coffeeMachine = CoffeeMachine(catalogue)
-    coffeeMachine.sell_coffee()
 
-def createCatalogue():
-    catalogue = dict()
-    offers = []
-    offers.append(Recipe("Capuccino", 0.65))
-    offers.append(Recipe("Hot chocolate", 0.50))
-    for r in offers:
-        catalogue[r.name] = r.to_string()
-    return catalogue
-    
-
-if __name__ == "__main__":
-    main()
---
+++ main.py
@@ -0,0 +1,20 @@
+from coffee_machine import CoffeeMachine
+from recipe import Recipe
+
+def main():
+    catalogue = createCatalogue()
+    coffeeMachine = CoffeeMachine(catalogue)
+    coffeeMachine.sell_coffee()
+
+def createCatalogue():
+    co_strength = ("strength",range(1,5), 3)
+    co_milk = ("milk", range(0,5), 0)
+    co_sugar = ("sugar", range(0,5), 0)
+    catalogue = []
+    catalogue.append(Recipe("Capuccino", 0.65, [co_strength, co_milk, co_sugar]))
+    catalogue.append(Recipe("Hot chocolate", 0.50, [co_milk, co_sugar]))
+    catalogue.append(Recipe("Hot water for tea", 0.25))
+    return catalogue
+
+if __name__ == "__main__":
+    main()
